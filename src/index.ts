46d0fdfe358983c929ff4136efeef96a1325a63a
Sadra Amlashi <amlashi.sadra@gmail.com>
2020-09-05 10:40:40 +0430

    Modify to export module in seprated way

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
-// import OneTimeActivationCode from './OneTimeActivationCode';
+import OneTimeActivationCode from './OneTimeActivationCode';
+import ReachedToAttemptsException from './exceptions/ReachedToAttempts.exception';
+import NotFoundKeyException from './exceptions/NotFoundKey.exception';
 
-export * from './OneTimeActivationCode';
+export default OneTimeActivationCode
 export { default as ReachedToAttemptsException } from './exceptions/ReachedToAttempts.exception';
 export { default as NotFoundKeyException } from './exceptions/NotFoundKey.exception';
+
+module.exports = OneTimeActivationCode;
+module.exports.ReachedToAttemptsException = ReachedToAttemptsException
+module.exports.NotFoundKeyException = NotFoundKeyException
