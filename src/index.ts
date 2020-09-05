bcb5877a479a4143f72dd08816859f4db42b07da
Sadra Amlashi <amlashi.sadra@gmail.com>
2020-09-05 10:41:15 +0430

    1.0.12

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,10 @@ import OneTimeActivationCode from './OneTimeActivationCode';
 import ReachedToAttemptsException from './exceptions/ReachedToAttempts.exception';
 import NotFoundKeyException from './exceptions/NotFoundKey.exception';
 
-export default OneTimeActivationCode
+export default OneTimeActivationCode;
 export { default as ReachedToAttemptsException } from './exceptions/ReachedToAttempts.exception';
 export { default as NotFoundKeyException } from './exceptions/NotFoundKey.exception';
 
 module.exports = OneTimeActivationCode;
-module.exports.ReachedToAttemptsException = ReachedToAttemptsException
-module.exports.NotFoundKeyException = NotFoundKeyException
+module.exports.ReachedToAttemptsException = ReachedToAttemptsException;
+module.exports.NotFoundKeyException = NotFoundKeyException;
